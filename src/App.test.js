5b7fb3e123d6a8359598a77cbcdf78f6ca92c0d4
KASHI TAMANG <tamang.kashi@gmail.com>
2022-06-09 15:15:17 -0700

    tweaking test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the word fight', (fight) => {
   render(<App />);
-  const linkElement = screen.getByText(/fight/i);
+  const linkElement = screen.getByText(fight);
   expect(linkElement).toBeInTheDocument();
 });
